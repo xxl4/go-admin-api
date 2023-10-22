c9ae09914678d3a426cbc55dc294eb8815d05ddc
steve <nice.lizhi@gmail.com>
2023-10-22 08:55:33 +0800

    fix shell restart

--- restart.sh
+++ restart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "go build"
 go mod tidy
-go build -o go-admin main.go
-chmod +x ./go-admin
-echo "kill go-admin service"
-killall go-admin # kill go-admin service
-nohup ./go-admin server -c=config/settings.dev.yml >> access.log 2>&1 & #后台启动服务将日志写入access.log文件
-echo "run go-admin success"
-ps -aux | grep go-admin
+go build -o go-admin-api main.go
+chmod +x ./go-admin-api
+echo "kill go-admin-api service"
+killall go-admin-api # kill go-admin service
+nohup ./go-admin-api server -c=config/settings.dev.yml >> access.log 2>&1 & #后台启动服务将日志写入access.log文件
+echo "run go-admin-api success"
+ps -aux | grep go-admin-api
